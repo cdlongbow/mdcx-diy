2c1c0fc2c3af7614b070cb612b109b8992def21e
cdlongbow <1730218335@qq.com>
2026-06-23 15:01:01 +0800

    Increment LOCAL_VERSION to 220260623

--- mdcx/consts.py
+++ mdcx/consts.py
@@ -3,7 +3,7 @@ import platform
 import sys
 from pathlib import Path
 
-LOCAL_VERSION = 220260622
+LOCAL_VERSION = 220260623
 
 GITHUB_REPO = "cdlongbow/mdcx-diy"
 GITHUB_RELEASES_URL = f"https://github.com/{GITHUB_REPO}/releases"
